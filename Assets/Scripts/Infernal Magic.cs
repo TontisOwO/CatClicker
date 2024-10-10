b95605cdd77413688a1fb8ab9daa5ecd42b9dc7d
Azure Biander <azure.biander@learnet.se>
2024-10-10 10:04:51 +0200

    Fuckery

---
+++ Assets/Scripts/Infernal Magic.cs
@@ -0,0 +1,28 @@
+using System;
+using System.Runtime.InteropServices;
+using UnityEngine;
+
+public class InfernalMagic : MonoBehaviour
+{
+
+    [DllImport("user32.dll")]
+    public static extern int MessageBox(IntPtr hWnd, string text, string caption, uint type);
+
+    [DllImport("user32.dll")]
+    private static extern IntPtr GetActiveScreen();
+    private struct MARGINS
+    {
+        public int cxLeftWidth;
+        public int cxRightWidth;
+        public int cxTopHeight;
+        public int cxBottomHeight;
+    }
+
+    [DllImport("Dwmapi.dll")]
+    private static extern uint DwmExtendFrameIntoClientArea(IntPtr hWnd, ref MARGINS margins);
+
+    private void Start()
+    {
+        MessageBox(new IntPtr(0), "Hello World", "Hello Windows", 0);
+    }
+}
