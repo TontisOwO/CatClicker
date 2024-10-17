16271b0bf8646d84758d2bf0ab936134b9b64ddf
Azure Biander <azure.biander@learnet.se>
2024-10-17 10:43:46 +0200

    Thing works

--- Assets/Scripts/Infernal Magic.cs
+++ Assets/Scripts/Infernal Magic.cs
@@ -10,25 +10,49 @@ public class InfernalMagic : MonoBehaviour
 
     [DllImport("user32.dll")]
     private static extern IntPtr GetActiveWindow();
+
+    [DllImport("user32.dll")]
+    private static extern int SetWindowLong(IntPtr hWnd,  int nIndex, uint dwNewLong);
+
+    [DllImport("user32.dll", SetLastError = true)]
+    static extern bool SetWindowPos(IntPtr hWnd, IntPtr hWndInsertAfter, int X, int Y, int cx, int cy, uint uFlags);
+
+    [DllImport("user32")]
+    static extern int SetLayeredWindowAttributes(IntPtr hWnd, uint crKey, byte bAlpha, uint dwFlags);
     private struct Margins
     {
-        public int cxLeftWidth;
-        public int cxRightWidth;
-        public int cxTopHeight;
-        public int cxBottomHeight;
+        public int Left;
+        public int Top;
+        public int Right;
+        public int Bottom;
     }
 
     [DllImport("Dwmapi.dll")]
     private static extern uint DwmExtendFrameIntoClientArea(IntPtr hWnd, ref Margins margins);
 
-    private void Start()
+    const int Gwl_ExStyle = -20;
+
+    const uint Ws_Ex_Layered = 0x00080000;
+    const uint Ws_Ex_Transparent = 0x00000020;
+
+    static readonly IntPtr HWnd_Topmost = new IntPtr(-1);
+
+    const uint Lwa_Colorkey = 0x00000001;
+
+    public void Start()
     {
-        MessageBox(new IntPtr(0), "Hello World", "Hello Windows", 0);
+#if !UNITY_EDITOR
+        MessageBox(new IntPtr(0), "hello world", "hello dialog", 0);
 
         IntPtr hWnd = GetActiveWindow();
 
-        Margins margins = new Margins { cxLeftWidth = -1};
-        DwmExtendFrameIntoClientArea (hWnd, ref margins);
-    }
+        Margins margins = new Margins { Left = -1};
+        DwmExtendFrameIntoClientArea(hWnd, ref margins);
 
+        SetWindowLong(hWnd, Gwl_ExStyle, Ws_Ex_Layered);
+        SetLayeredWindowAttributes(hWnd, 0,0, Lwa_Colorkey);
+        
+        SetWindowPos(hWnd, HWnd_Topmost, 0 ,0 ,0 ,0 ,0);
+#endif
+    }
 }
